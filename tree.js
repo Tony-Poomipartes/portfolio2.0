efd7050f0a2178e027ff81d9a98a0c97e4c2daba
Tony-Poomipartes <tony.p.pro@gmail.com>
2023-07-09 01:05:53 +0200

    ✨feat(tree.js):
    add a function to consol.log a text version tree of an application

---
+++ tree.js
@@ -0,0 +1,40 @@
+import { readdirSync, statSync } from 'fs';
+import { join } from 'path';
+
+function generateTree(directoryPath, prefix = '') {
+  // Get the list of files in the directory
+  const files = readdirSync(directoryPath);
+  // Initialize an empty string to store the tree structure
+  let tree = '';
+
+  // Iterate over each file in the directory
+  files.forEach((file, index) => {
+    // Get the full path of the file
+    const filePath = join(directoryPath, file);
+    // Get the stats of the file
+    const stats = statSync(filePath);
+    // Check if the file is a directory
+    const isDirectory = stats.isDirectory();
+
+    // Add the current file to the tree structure with the appropriate indentation
+    tree += prefix + (index === files.length - 1 ? '└── ' : '├── ') + file + '\n';
+
+    // If the file is a directory, recursively generate the tree for the nested directory
+    if (isDirectory) {
+      // Update the prefix for the nested directory
+      const nestedPrefix = prefix + (index === files.length - 1 ? '    ' : '│   ');
+      // Generate the tree for the nested directory and append it to the current tree structure
+      tree += generateTree(filePath, nestedPrefix);
+    }
+  });
+
+  // Return the generated tree structure
+  return tree;
+  // Note: The following line of code will not be executed as it is placed after the return statement
+  console.log(tree);
+}
+
+const projectPath = '/home/student/home/student/Bureau/html/projet perso/portfolio2.0/src';
+const tree = generateTree(projectPath);
+
+console.log(tree);
